9b5c0b45fbcda5b239fb1745eb28d8da7d669a82
ShinoBiWan <shinobione@users.noreply.github.com>
2026-08-11 22:57:08 +0200

    switch app entry to V0.1.2 quality-first shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './AppV012';
 import './styles.css';
 import './subscription-bridge.css';
 import './v0.1.1.css';
+import './v0.1.2.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
